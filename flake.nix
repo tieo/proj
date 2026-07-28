e8dfd2412b01ae2e39fbe5fc6735886f6763e7c8
tieo <65707274+tieo@users.noreply.github.com>
2026-07-28 17:40:37 +0200

    book: take viewbook where a view is an address

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
           pname = "proj";
           version = "0.1.0";
           src = ./.;
-          vendorHash = "sha256-lU1Ew7AyuoYRGpgVPr9ew6VVxRUttQfO+qx/RCnjQbU=";
+          vendorHash = "sha256-G/mcs7X9Ph1ZWrutwaqpZpuCpCzRc0APP5Rd8A/QKbY=";
           subPackages = [ "cmd/proj" ];
           postInstall = ''
             install -Dm0644 -t $out/share/proj shells/proj.zsh shells/proj.bash shells/proj.fish
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/BurntSushi/toml v1.4.0
 	github.com/mattn/go-runewidth v0.0.24
 	github.com/spf13/cobra v1.8.1
-	github.com/tieo/viewbook v0.0.0-20260728152655-121e4269be49
+	github.com/tieo/viewbook v0.0.0-20260728153825-991d90c00691
 )
 
 require (
